95e018eb0588ba7c226f2427460edbc1aab72e6a
Jason An <an.jason.1994@gmail.com>
2017-04-05 16:18:30 -0700

    (feat) Initialize baseline files

---
+++ schema.sql
@@ -0,0 +1,9 @@
+DROP DATABASE IF EXISTS thesis;
+
+CREATE DATABASE thesis;
+
+USE thesis;
+
+/*  Execute this file from the command line by typing:
+ *    mysql -u root < server/schema.sql
+ *  to create the database and the tables.*/
