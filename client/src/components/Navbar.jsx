9be0c0b029dea9d9b21c08b42b26d650d5cbbdfc
Jason An <an.jason.1994@gmail.com>
2017-04-07 15:55:17 -0700

    (feat) Separate Navbar into component

---
+++ client/src/components/Navbar.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import {
+  BrowserRouter as Router,
+  Route,
+  Link
+} from 'react-router-dom';
+
+const Navbar = () => {
+  return (
+    <ul>
+      <li><Link to="/">Login</Link></li>
+      <li><Link to="/Balance">Balance</Link></li>
+      <li><Link to="/budget">Budget</Link></li>
+      <li><Link to="/ccSelector">CC Selector</Link></li>
+      <li><Link to="/portfolio">Portfolio</Link></li>
+      <li><Link to="/emailNotifications">Email Notifications</Link></li>
+      <li><Link to="/">Logout</Link></li>
+    </ul>
+  );
+};
+
+export default Navbar;
