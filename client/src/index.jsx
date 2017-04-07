3819d7267a717ba8442f1019dc000e90e23da1b7
Jason An <an.jason.1994@gmail.com>
2017-04-07 15:55:17 -0700

    (feat) Separate Navbar into component

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -5,16 +5,17 @@ import {
   Route,
   Link
 } from 'react-router-dom';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
 
+import appReducer from './reducers/appReducer.js';
 import Balance from './components/Balance.jsx';
 import Budget from './components/Budget.jsx';
 import CCSelector from './components/CCSelector.jsx';
 import Login from './components/Login.jsx';
+import Navbar from './components/Navbar.jsx';
 import Portfolio from './components/Portfolio.jsx';
 import EmailNotifications from './components/EmailNotifications.jsx';
-import { createStore } from 'redux';
-import { Provider } from 'react-redux';
-import appReducer from './reducers/appReducer.js';
 
 class App extends React.Component {
   constructor(props) {
@@ -30,26 +31,17 @@ class App extends React.Component {
     return (
       <Router>
         <div>
-        <h1>Thesis</h1>
-        <ul>
-          <li><Link to="/">Login</Link></li>
-          <li><Link to="/Balance">Balance</Link></li>
-          <li><Link to="/budget">Budget</Link></li>
-          <li><Link to="/ccSelector">CC Selector</Link></li>
-          <li><Link to="/portfolio">Portfolio</Link></li>
-          <li><Link to="/emailNotifications">Email Notifications</Link></li>
-          <li><Link to="/">Logout</Link></li>
-        </ul>
+          <h1>Thesis</h1>
+          <Navbar />
 
-        <hr/>
+          <hr/>
 
-        <Route exact path="/" component={Login}/>
-        <Route path="/balance" component={Balance}/>
-        <Route path="/budget" component={Budget}/>
-        <Route path="/ccSelector" component={CCSelector}/>
-        <Route path="/portfolio" component={Portfolio}/>
-        <Route path="/emailNotifications" component={EmailNotifications}/>
-
+          <Route exact path="/" component={Login}/>
+          <Route path="/balance" component={Balance}/>
+          <Route path="/budget" component={Budget}/>
+          <Route path="/ccSelector" component={CCSelector}/>
+          <Route path="/portfolio" component={Portfolio}/>
+          <Route path="/emailNotifications" component={EmailNotifications}/>
         </div>
       </Router>
     );
