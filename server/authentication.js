b69b07c9a02fa803137bf0deead50de4a3356a8b
Jason An <an.jason.1994@gmail.com>
2017-04-10 17:32:36 -0700

    (refactor) Reintroduce env plaid configuration

--- server/authentication.js
+++ server/authentication.js
@@ -1,19 +1,19 @@
 var passport = require('passport');
-var config = require('./../config/config');
+// var config = require('./../config/config');
 var FacebookStrategy = require('passport-facebook');
 var db = require('./../database/index');
 
 passport.use(new FacebookStrategy({
-//   clientID: process.env.FB_appID,
-//   clientSecret: process.env.FB_secret,
-//   callbackURL: process.env.SITE_URL + 'auth/facebook/return',
-//   profileFields: ['id', 'displayName', 'email'],
-// }, 
-  clientID: config.facebook.clientID,
-  clientSecret: config.facebook.clientSecret,
-  callbackURL: config.facebook.callbackURL,
+  clientID: process.env.FB_appID,
+  clientSecret: process.env.FB_secret,
+  callbackURL: process.env.SITE_URL + 'auth/facebook/return',
   profileFields: ['id', 'displayName', 'email'],
 }, 
+//   clientID: config.facebook.clientID,
+//   clientSecret: config.facebook.clientSecret,
+//   callbackURL: config.facebook.callbackURL,
+//   profileFields: ['id', 'displayName', 'email'],
+// }, 
 (accessToken, refreshToken, profile, done) => {
     db.findUser(profile._json.id, (err, existingUser) => {
       // TODO: error handle invalid search for profile.id
