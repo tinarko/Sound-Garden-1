a07ac7d3f302dca3602a79fb75a0f6d9e47c6257
Jason An <an.jason.1994@gmail.com>
2017-04-10 17:33:40 -0700

    (bug) minor plaid fix

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import config from './../../../config/config.js';
+// import config from './../../../config/config.js';
 
 class Login extends React.Component {
   constructor(props) {
