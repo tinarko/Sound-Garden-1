a0140ad2f4f46855229fe3f3d81ec12e6cb0510d
Jason An <an.jason.1994@gmail.com>
2017-04-11 17:38:09 -0700

    (feat) Implement Balance display

--- client/src/components/BalanceListEntry.jsx
+++ client/src/components/BalanceListEntry.jsx
@@ -7,7 +7,9 @@ const BalanceListEntry = (props) => {
         return (
           <div>
             <h5>{account.account.name}</h5>
-            <h5 className={(props.name==='credit' || props.mortgage==='mortgage') ? "balance-list-credit" : ""}>{account.account.balances.available || account.account.balances.current}</h5>
+            <h5 className={(props.name==='credit' || props.name==='mortgage' || props.account.account.balances.available < 0) ? "balance-list-credit" : ""}>
+              {account.account.balances.available || account.account.balances.current}
+            </h5>
           </div>
         );
       })}
