80a556bfc16c034f117dcf629f872b65322ebcb4
Christopher Yang <chrisyang190@gmail.com>
2017-04-12 12:23:30 -0700

    (cleanup) Minor Cleanup

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -2,7 +2,6 @@ import { combineReducers } from 'redux';
 import balance from './balance.js';
 import budget from './budget.js';
 import creditcards from './creditcards';
-import transactions from './transactions';
 
 //implement lines below when there are actually other reducers in folder
 // import * as reducers from './reducers';
