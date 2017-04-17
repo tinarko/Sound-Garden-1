886ef04d25070d30e5a7129e7d1a66945ea0d6b7
Jason An <an.jason.1994@gmail.com>
2017-04-16 22:53:35 -0700

    (feat) Implement transactions table

--- client/src/components/TransactionsTable.jsx
+++ client/src/components/TransactionsTable.jsx
@@ -1,12 +1,50 @@
-import React from 'react';
+import React, {Component} from 'react';
+import DataTables from 'material-ui-datatables';
 
-const TransactionsTable = () => {
-  console.log(this.props);
-  return (
-    <div>
-
-    </div>
-  );
-};
-
+class TransactionsTable extends Component {
+  constructor(props) {
+    super(props);
+  }
+  
+  render() {
+    console.log('inside the data table-------', this.props);
+    const tableColumns = [
+      {
+        key: 'name',
+        label: 'Transaction'
+      }, {
+        key: 'amount',
+        label: 'Amount'
+      }, {
+        key: 'institution_name',
+        label: 'Bank'
+      }, {
+        key: 'date',
+        label: 'Date'
+      }
+    ];
+    const tableData = [];
+    this.props.data.forEach((value) => {
+      tableData.push({
+        name: value.name,
+        amount: value.amount,
+        institution_name: value.institution_name,
+        date: value.date.toString().slice(0,10),
+      });
+    });
+    console.log('table data', tableData)
+    return (
+      <div className="transactions-table">
+        <DataTables
+          height={'auto'}
+          selectable={false}
+          showRowHover={false}
+          columns={tableColumns}
+          data={tableData}
+          showCheckboxes={false}
+        />
+      </div>
+    );
+  }
+}
 export default TransactionsTable;
