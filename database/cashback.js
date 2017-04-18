7e29e85d2efc2a9167d956cb9392cf9173ad9a5a
Tina Ko <tinarchan@gmail.com>
2017-04-17 18:54:48 -0700

    (refactor) modularize db

---
+++ database/cashback.js
@@ -0,0 +1,49 @@
+var mysql = require('mysql');
+var db = require ('./index.js');
+
+var connection = db.connection;
+
+exports.changeCashbackCategories = (catid, percent, action, cb) => {
+  var updatedPercent;
+  if (action === 'increment') {
+    updatedPercent = percent + 0.5;
+  } else {
+    updatedPercent = percent - 0.5;
+  }
+  var params = [updatedPercent, catid];
+  var query = 'update cccategories set value = ? where id = ?';
+
+  connection.query(query, params, (err, results) => {
+    if (err) {
+      cb(err, null);
+    } else {
+      cb(null, results);
+    }
+  });
+};
+
+exports.createCashbackCategory = function(ccid, name, percent, cb) {
+  var query = 'INSERT INTO cccategories (categoryname, value, ccid) VALUES (?, ?, ?)';
+
+  var params = [name, percent, ccid];
+
+  connection.query(query, params, (err, results) => {
+    if (err) {
+      cb(err, null);
+    } else {
+      cb(null, results);
+    }
+  });
+};
+
+exports.deleteCashbackCategory = function(catid, cb) {
+  var query = 'delete from cccategories where id = ?';
+
+  connection.query(query, catid, (err, results) => {
+    if (err) {
+      cb(err, null);
+    } else {
+      cb(null, results);
+    }
+  });
+};
