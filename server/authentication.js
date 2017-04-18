08edd35f307894d6f9762b631ab4c23cdc1427c0
Jason An <an.jason.1994@gmail.com>
2017-04-18 14:57:41 -0700

    (refactor) Remove unncessary comments in authentication

--- server/authentication.js
+++ server/authentication.js
@@ -1,37 +1,6 @@
 var passport = require('passport');
 var Auth0Strategy = require('passport-auth0');
 var db = require('./../database/index');
-// var config = require('./../config/config');
-// var FacebookStrategy = require('passport-facebook');
-
-// passport.use(new FacebookStrategy({
-//   clientID: process.env.FB_appID,
-//   clientSecret: process.env.FB_secret,
-//   callbackURL: process.env.SITE_URL + 'auth/facebook/return',
-//   profileFields: ['id', 'displayName', 'email'],
-// }, 
-// //   clientID: config.facebook.clientID,
-// //   clientSecret: config.facebook.clientSecret,
-// //   callbackURL: config.facebook.callbackURL,
-// //   profileFields: ['id', 'displayName', 'email'],
-// // }, 
-// (accessToken, refreshToken, profile, done) => {
-//     db.findUser(profile._json.id, (err, existingUser) => {
-//       // TODO: error handle invalid search for profile.id
-//       if (existingUser !== false) {
-//         console.log('this is if existing user', existingUser);
-//         done(null, profile);
-//       } else {
-//         console.log('this is if NOT existinguser');
-//         db.saveUser(profile._json, (err, newUser) => {
-//           if (err) {
-//             // TODO: error handle invalid save of user
-//           }
-//           done(null, profile);
-//         });
-//       }
-//     });
-// }));
 
 passport.use(new Auth0Strategy({
   domain: 'hrsf72-thesis.auth0.com',
@@ -57,8 +26,6 @@ passport.use(new Auth0Strategy({
   });
 }));
 
-// serialize and deserialize User to save and retrieve user data from session
-// stores user in session
 passport.serializeUser((user, done) => {
   done(null, user);
 });
