56e5e22400cf26e4d7c92d7eaaf0df458b51eb46
Jason An <an.jason.1994@gmail.com>
2017-04-20 12:06:23 -0700

    (refactor) Functional stateless component Login

---
+++ client/src/actions/plaid.js
@@ -0,0 +1,39 @@
+import config from './../../../config/config.js';
+
+module.exports.createPlaid = () => {
+  Plaid.create({
+    clientName: 'Plaid Walkthrough Demo',
+    env: config.plaid.plaidEnv,
+    key: config.plaid.publicKey,
+    product: ['auth', 'transactions'],
+    // webhook: '[WEBHOOK_URL]', // Optional – use webhooks to get transaction and error updates
+    // selectAccount: true, // Optional – trigger the Select Account
+    onLoad: function() {
+    },
+    onSuccess: function(public_token, metadata) {
+      fetch('/plaid/access_token', {
+        method: 'POST',
+        // TODO: required to send cookies
+        credentials: 'same-origin',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+          public_token: public_token,
+          metadata: metadata,
+        })
+      })
+        .then((response) => {
+          console.log('successful post to plaid access token');
+        })
+        .catch((err) => {
+          console.log('error in post to plaid access token', err);
+        });
+    },
+    onExit: function(err, metadata) {
+      console.log(metadata);
+      if (err != null) {
+      }
+    }
+  }).open();
+};
--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -1,64 +1,19 @@
 import React from 'react';
-import config from './../../../config/config.js';
 import RaisedButton from 'material-ui/RaisedButton';
+import { createPlaid } from './../actions/plaid.js';
 
-class Login extends React.Component {
-  constructor(props) {
-    super(props);
-    this.onPlaidClick = this.onPlaidClick.bind(this);
-  }
+const Login = (props) => {
+  return (
+    <div className="login">
+      <h1>Welcome to Financial Advisorly!</h1>
+      <h3>You are well on your way to saving big</h3>
 
-  onPlaidClick() {
-    Plaid.create({
-      clientName: 'Plaid Walkthrough Demo',
-      env: config.plaid.plaidEnv,
-      key: config.plaid.publicKey,
-      product: ['auth', 'transactions'],
-      // webhook: '[WEBHOOK_URL]', // Optional – use webhooks to get transaction and error updates
-      // selectAccount: true, // Optional – trigger the Select Account
-      onLoad: function() {
-      },
-      onSuccess: function(public_token, metadata) {
-        fetch('/plaid/access_token', {
-          method: 'POST',
-          // TODO: required to send cookies
-          credentials: 'same-origin',
-          headers: {
-            'Content-Type': 'application/json'
-          },
-          body: JSON.stringify({
-            public_token: public_token,
-            metadata: metadata,
-          })
-        })
-          .then((response) => {
-            console.log('successful post to plaid access token');
-          })
-          .catch((err) => {
-            console.log('error in post to plaid access token', err);
-          });
-      },
-      onExit: function(err, metadata) {
-        console.log(metadata);
-        if (err != null) {
-        }
-      }
-    }).open();
-  }
-
-  render() {
-    return (
-      <div className="login">
-        <h1>Welcome to Financial Advisorly!</h1>
-        <h3>You are well on your way to saving big</h3>
-
-        <p>Add your bank accounts:</p>
-        <RaisedButton id="link-button"
-          onClick={this.onPlaidClick} label="Add Bank Accounts" 
-        />
-      </div>
-    );
-  }
-}
+      <p>Add your bank accounts:</p>
+      <RaisedButton id="link-button"
+        onClick={createPlaid} label="Add Bank Accounts" 
+      />
+    </div>
+  );
+};
 
 export default Login;
