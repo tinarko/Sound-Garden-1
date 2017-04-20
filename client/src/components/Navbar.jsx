aa0e3d51aadd0283b59248a5aafe5b1a8b208043
Jason An <an.jason.1994@gmail.com>
2017-04-20 12:11:10 -0700

    (feat) Create plaid addition button on Navbar

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -4,6 +4,7 @@ import {
   Route,
   Link
 } from 'react-router-dom';
+import { createPlaid } from './../actions/plaid.js';
 import {Toolbar, ToolbarGroup, ToolbarSeparator, ToolbarTitle} from 'material-ui/Toolbar';
 import FlatButton from 'material-ui/FlatButton';
 
@@ -13,11 +14,12 @@ const Navbar = (props) => {
   if (document.cookie.replace(/(?:(?:^|.*;\s*)advisorly\s*\=\s*([^;]*).*$)|^.*$/, "$1")) {
     view = (
       <div>
+        <FlatButton label="Add Account" onClick={createPlaid} containerElement={<Link to ="/"/>} />
         <FlatButton label="Balance" containerElement={<Link to="/Balance" />} />
         <FlatButton label="Budget" containerElement={<Link to="/budget" />} />
         <FlatButton label="Cashback" containerElement={<Link to="/ccCashback" />} />
         <FlatButton label="Transactions" containerElement={<Link to="/transactions" />} />
-        <a href="/auth/logout"><FlatButton label="Logout" /></a>
+        <a href="/auth/logout"><FlatButton label="Logout" containerElement={<div/>} /></a>
       </div>
     );
   } else {
