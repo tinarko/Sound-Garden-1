189361862cd5b801fb4dadd638a24464f527c5d8
Christopher Yang <chrisyang190@gmail.com>
2017-04-22 12:15:08 -0700

    (feat) Initialize Server and DB Test

--- spec/backend-spec.js
+++ spec/backend-spec.js
@@ -8,46 +8,57 @@ const chaiHttp = require ('chai-http');
 const server = require('../server/index');
 const mysql = require('mysql');
 
-var port = process.env.PORT || 1337;
+// var port = process.env.PORT || 1337;
 
 chai.use(chaiHttp);
 
 describe ('', function () {
 
+  let db;
+  // let port = process.env.PORT || 1337;
+
+
   beforeEach( () => {
-    var connection = mysql.createConnection({
-      // host: 'localhost',
+    db = mysql.createConnection({
       user: 'root',
       password: '',
       database: 'heroku_aa9603bdcb7e15e'
     });
-    connection.connect();
+    db.connect();
    // app.listen(port, function() {
     //   console.log('listening on port' + port);
     // });
+    afterEach(function () { server.close(); });
+  
   });
 
+  describe('Database Tests', () => {
 
-});
+    it('should have a users table', (done) => {
+      var queryString = 'select * from users;';
+      // var queryString = 'insert into users (userid, name, email) value(?, ?, ?)';
+      db.query(queryString, (err, results) => {
+        if (err) {
+          return done(err); 
+        }
+        console.log('results', results);
+        expect(results).to.exist;
+        done();
 
-describe('Server Tests', () => {
-
-  it('should return status of 404 if route is unknown', () => {
-    chai.request(server)
-    .get('/errorroute')
-    .end((err, res) => {
-      console.log('res.status', res.status);
-      expect(res.status).to.equal(404);
-    
+      });
     });
   });
-});
 
-describe('Database Tests', () => {
-  // it('should have a users table', () => {
-  //   var queryString = 'insert into users (userid, name, email) value(?, ?, ?)';
-  //   connection.query(queryString, ['facebook|123', 'Charles Xavier', 'profx@xavier.com'], () => {
-      
-  //   });
-  // });
+  describe('Server Tests', () => {
+
+    it('should return status of 404 if route is unknown', () => {
+      chai.request(server)
+      .get('/errorroute')
+      .end((err, res) => {
+        console.log('res.status', res.status);
+        expect(res.status).to.equal(404);
+      });
+    });
+  });
+
 });
