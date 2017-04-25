daa3a04735597188de45d84b0387c8a50916fec2
Jason An <an.jason.1994@gmail.com>
2017-04-25 15:52:03 -0700

    (feat) introduce plaid handler module

---
+++ server/requestHandlers/utility/plaidUtility.js
@@ -0,0 +1,40 @@
+var plaid = require('plaid');
+var Promise = require('bluebird');
+var PLAID_CLIENT_ID  = process.env.PLAID_clientID;
+var PLAID_SECRET     = process.env.PLAID_clientSecret;
+var PLAID_PUBLIC_KEY = process.env.PLAID_publicKey;
+var PLAID_ENV        = process.env.PLAID_env;
+
+var client = new plaid.Client(
+  PLAID_CLIENT_ID,
+  PLAID_SECRET,
+  PLAID_PUBLIC_KEY,
+  // TODO: adjust environment as product life cycle advances
+  plaid.environments[PLAID_ENV]
+);
+
+module.exports.promisePlaid = function(dbResponse, reqType, cb) {
+  var promises = dbResponse.map(function(item) {
+    // type of plaid request
+    return client[type](item.access_token)
+      .then(function(data) {
+        data.accounts.forEach(function(account) {
+          // may be a jamming point
+          account.instution_name = item.institution_name;
+        });
+        return data.accounts;
+      })
+      .catch(function(error) {
+        return error;
+      });
+  });
+  Promise.map(promises, function(asyncResult) {
+    return asyncResult;
+  })
+    .then(function(results) {
+      cb(null, results);
+    })
+    .catch(function(error) {
+      cb(error, null);
+    });
+}; 
