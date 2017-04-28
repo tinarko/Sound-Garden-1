af74b5caa37f6f76f879f6307ddc4bb3cd20f722
Jason An <an.jason.1994@gmail.com>
2017-04-27 18:52:08 -0700

    (refactor) Remove files

--- client/src/components/Balance/BalanceTable.jsx
+++ client/src/components/Balance/BalanceTable.jsx
@@ -3,29 +3,29 @@ import {Table, TableBody, TableHeader, TableHeaderColumn, TableRow, TableRowColu
 
 const BalanceTable = (props) => {
   return (
-        <div className="balance-table">
-          <Table>
-            <TableHeader displaySelectAll={false} adjustForCheckbox={false}>
-              <TableRow>
-                <TableHeaderColumn>Account Name</TableHeaderColumn>
-                <TableHeaderColumn>Type</TableHeaderColumn>
-                <TableHeaderColumn>Balance</TableHeaderColumn>
-              </TableRow>
-            </TableHeader>
-            <TableBody 
-              displayRowCheckbox={false}
-              stripedRows={true}
-            >
-              {props.balance.map((item, index) => {
-                return (<TableRow>
-                  <TableRowColumn>{item.institution_name + ' ' + item.name}</TableRowColumn>
-                  <TableRowColumn>{`$${item.balances.available || item.balances.current}`}</TableRowColumn>
-                  <TableRowColumn>{item.subtype === 'cd' ? 'CD' : item.subtype[0].toUpperCase() + item.subtype.substring(1)}</TableRowColumn>
-                </TableRow>);
-              })}
-            </TableBody>
-          </Table>
-        </div>);
+    <div className="balance-table">
+      <Table>
+        <TableHeader displaySelectAll={false} adjustForCheckbox={false}>
+          <TableRow>
+            <TableHeaderColumn>Account Name</TableHeaderColumn>
+            <TableHeaderColumn>Type</TableHeaderColumn>
+            <TableHeaderColumn>Balance</TableHeaderColumn>
+          </TableRow>
+        </TableHeader>
+        <TableBody 
+          displayRowCheckbox={false}
+          stripedRows={true}
+        >
+          {props.balance.map((item, index) => {
+            return (<TableRow>
+              <TableRowColumn>{item.institution_name + ' ' + item.name}</TableRowColumn>
+              <TableRowColumn>{`$${item.balances.available || item.balances.current}`}</TableRowColumn>
+              <TableRowColumn>{item.subtype === 'cd' ? 'CD' : item.subtype[0].toUpperCase() + item.subtype.substring(1)}</TableRowColumn>
+            </TableRow>);
+          })}
+        </TableBody>
+      </Table>
+    </div>);
 };
 
 export default BalanceTable;
--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -7,10 +7,6 @@ import mapcalculator from './mapcalculator.js';
 import login from './login.js';
 import navbar from './navbar.js';
 
-//implement lines below when there are actually other reducers in folder
-// import * as reducers from './reducers';
-// const appReducer = combineReducers(reducers);
-
 export default combineReducers({
   balance,
   budget,
