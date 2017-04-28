4a20e92a1384100ba13aa2f71beee078371c5438
Tina Ko <tinarchan@gmail.com>
2017-04-28 12:12:53 -0700

    (styling) rebase and change font sizes

--- client/src/components/Balance/BalanceTable.jsx
+++ client/src/components/Balance/BalanceTable.jsx
@@ -15,9 +15,9 @@ const BalanceTable = (props) => {
         >
           {props.balance.map((item, index) => {
             return (<TableRow>
-              <TableRowColumn style={{fontSize: '16', color: '#000', fontFamily: 'Roboto'}}>{item.institution_name + ' ' + item.name}</TableRowColumn>
-              <TableRowColumn style={{fontSize: '16', color: '#000', fontFamily: 'Roboto'}}>{`$${item.balances.available || item.balances.current}`}</TableRowColumn>
-              <TableRowColumn style={{fontSize: '16', color: '#000', fontFamily: 'Roboto'}}>{item.subtype === 'cd' ? 'CD' : item.subtype[0].toUpperCase() + item.subtype.substring(1)}</TableRowColumn>
+              <TableRowColumn style={{fontSize: '18', color: '#000', fontFamily: 'Roboto'}}>{item.institution_name + ' ' + item.name}</TableRowColumn>
+              <TableRowColumn style={{fontSize: '18', color: '#000', fontFamily: 'Roboto'}}>{`$${item.balances.available || item.balances.current}`}</TableRowColumn>
+              <TableRowColumn style={{fontSize: '18', color: '#000', fontFamily: 'Roboto'}}>{item.subtype === 'cd' ? 'CD' : item.subtype[0].toUpperCase() + item.subtype.substring(1)}</TableRowColumn>
             </TableRow>);
           })}
         </TableBody>
